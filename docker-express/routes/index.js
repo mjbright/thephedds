8b3ca8fa7d821958e53d09f746ec190a66519250
thenayr <iamnayr@gmail.com>
2015-06-21 01:19:49 -0700

    Added express app to iframe

---
+++ docker-express/routes/index.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var router = express.Router();
+var server = require('http').Server(express);
+var DeepstreamClient = require( 'deepstream.io-client-js' );
+var bodyParser = require('body-parser');
+client = DeepstreamClient( '172.17.42.1:6021' )
+
+
+
+client.login({ username: 'ds-example' }, function( success ){
+  if( success ) {
+    doIt();
+  }
+});
+
+function doIt() {
+  console.log("logged in");
+}
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+
+router.post('/docker-start', function(req, res) {
+  res.send('success');
+  console.log(req.body);
+  client.event.emit( 'docker', req.body);
+})
+
+
+module.exports = router;
